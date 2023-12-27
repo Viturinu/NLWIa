d928b0cb3b19b2ea2a5c7f1292f6efb798e6bbef
Victor Oliveira Almeida <victor.almeida.ti@gmail.com>
2023-12-27 19:27:52 -0300

    Segunda aula em 43 minutos

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,8 +1,26 @@
+import { Github } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
 
 export function App() {
 
   return (
-    <Button>Teste</Button>
-  )
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">Desenvovlido com 💜 no NLW do RocketSeat</span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+      <main className="bg-red-500 flex-1">
+        oi
+      </main>
+    </div>)
 }
